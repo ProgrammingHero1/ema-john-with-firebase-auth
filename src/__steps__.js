fbade6a6793ea2eddcfe97b8e9f1fcaa306c4a23
Programming Hero <programming.hero1@gmail.com>
2023-04-15 11:04:45 -0500

    show and hide password

--- src/__steps__.js
+++ src/__steps__.js
@@ -14,3 +14,25 @@
  * 4. set context value
  * 5. set the provider 
 */
+
+/**
+ * HOSTING
+ * --------------------
+ * One time per PC
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 
+ * For each project one time
+ * 1. firebase init
+ * 2. proceed 
+ * 3. hosting: firebase (up and down arrow) use space bar to select
+ * 4. existing project 
+ * 5. select the project careful
+ * 6. which project as public directory: dist
+ * 7. single page application: yes
+ * 8. continuous deployment: no
+ * 
+ * For every time deploy
+ * 1. npm run build
+ * 2. firebase deploy
+*/
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 
 const Login = () => {
+    const [show, setShow] = useState(false);
+
     const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
@@ -42,7 +44,12 @@ const Login = () => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="" required />
+                    <input type={show ? "text" : "password"} name="password" id="" required />
+                    <p onClick={() => setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span>: <span>Show Password</span>
+                        }
+                        </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
