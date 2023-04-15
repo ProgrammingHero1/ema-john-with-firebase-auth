f1083561332adc1cd21f0b1640721cec2789ec8a
Programming Hero <programming.hero1@gmail.com>
2023-04-14 23:07:59 -0500

    style login and signup view

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,8 +3,23 @@ import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Please Sign Up</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Confirm Password</label>
+                    <input type="password" name="confirm" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
         </div>
     );
 };
